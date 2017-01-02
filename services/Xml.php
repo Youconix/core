7c760c1e8c2f9c2e770c1e1b2a739c661d500e72
Roxanna Scheijen-Lugtigheid <roxannalugtigheid@gmail.com>
2017-01-02 21:58:41 +0100

    Added validation function to XML Service.

--- services/Xml.php
+++ services/Xml.php
@@ -40,14 +40,14 @@ class Xml extends Service
     public function createDocument($s_encoding = 'iso-8859-1', $bo_skipXPath = false)
     {
         $this->dom_document = new \DOMDocument('1.0', $s_encoding);
-        
+
         // We don't want to bother with white spaces
         $this->dom_document->preserveWhiteSpace = false;
-        
+
         $this->dom_document->resolveExternals = true; // for character entities
-        
+
         $this->dom_document->formatOutput = true; // keep output alignment
-        
+
         if (! $bo_skipXPath) {
             $this->obj_document = new \DOMXPath($this->dom_document);
         }
@@ -65,33 +65,33 @@ class Xml extends Service
     public function load($s_file, $s_encoding = 'iso-8859-1')
     {
         $this->createDocument($s_encoding, true);
-        
+
         if (! $this->dom_document->Load($s_file)) {
             throw new \IOException("Can not load XML-file " . $s_file . ". Check the address");
         }
-        
+
         $this->obj_document = new \DOMXPath($this->dom_document);
     }
 
     public function loadXML($s_content, $s_encoding = 'UTF-8')
     {
         $this->createDocument($s_encoding, true);
-        
+
         if (! $this->dom_document->loadXML($s_content)) {
             throw new \IOException("Can not load XML content. Content may be invalid.");
         }
-        
+
         $this->obj_document = new \DOMXPath($this->dom_document);
     }
 
     public function loadHTML($s_content, $s_encoding = 'UTF-8')
     {
         $this->createDocument($s_encoding, true);
-        
+
         if (! $this->dom_document->loadHTML($s_content)) {
             throw new \IOException("Can not load HTML content. Content may be invalid.");
         }
-        
+
         $this->obj_document = new \DOMXPath($this->dom_document);
     }
 
@@ -106,22 +106,22 @@ class Xml extends Service
     public function get($s_path)
     {
         $s_path = $this->getRealPath($s_path);
-        
+
         if (is_null($this->obj_document)) {
             throw new \XMLException("No document loaded.");
         }
-        
+
         $a_return = $this->obj_document->query("//" . $s_path);
-        
+
         if ($a_return->length < 1) {
             /* Part not found */
             throw new \XMLException("Can not find " . $s_path);
         }
-        
+
         foreach ($a_return as $entry) {
             $s_return = $entry->textContent;
         }
-        
+
         return $s_return;
     }
 
@@ -137,18 +137,18 @@ class Xml extends Service
     public function getBlock($s_path)
     {
         $s_path = $this->getRealPath($s_path);
-        
+
         if (is_null($this->obj_document)) {
             throw new \XMLException("No document loaded.");
         }
-        
+
         $a_return = $this->obj_document->query("//" . $s_path);
-        
+
         if ($a_return->length < 1) {
             /* Part not found */
             throw new \XMLException("Can not find " . $s_path);
         }
-        
+
         return $a_return;
     }
 
@@ -164,19 +164,19 @@ class Xml extends Service
     public function set($s_path, $s_content)
     {
         \youconix\core\Memory::type('String', $s_content);
-        
+
         $s_path = $this->getRealPath($s_path);
-        
+
         $a_data = explode('/', $s_path);
         $s_name = end($a_data);
         $a_return = $this->obj_document->query("//" . $s_path);
-        
+
         if ($a_return->length < 1) {
             /* Part not found */
             throw new \XMLException("Can not find " . $s_path);
         }
         $oldnode = $a_return->item(0);
-        
+
         $newNode = $this->dom_document->createElement($s_name);
         $newNode->appendChild($this->dom_document->createCDataSection($s_content));
         $oldnode->parentNode->replaceChild($newNode, $oldnode);
@@ -195,21 +195,21 @@ class Xml extends Service
     {
         \youconix\core\Memory::type('String', $s_path);
         \youconix\core\Memory::type('String', $s_content);
-        
+
         if ($this->exists($s_path)) {
             throw new \XMLException("Can not add existing " . $s_path);
         }
-        
+
         if (substr($s_path, 0, 9) == 'settings/') {
             $s_path = substr($s_path, 9);
         }
-        
+
         $s_parent = substr($s_path, 0, strrpos($s_path, '/'));
         $a_path = explode('/', $s_path);
         $s_name = end($a_path);
-        
+
         $this->addBlocks($s_parent);
-        
+
         $element = $this->getBlock($s_parent);
         $element = $element->item(0);
         $node = $this->dom_document->createElement($s_name, $s_content);
@@ -219,25 +219,25 @@ class Xml extends Service
     protected function addBlocks($s_path)
     {
         $a_path = explode('/', $s_path);
-        
+
         $i_length = count($a_path);
         $s_lastPath = '';
         $s_path = '';
         for ($i = 0; $i < $i_length; $i ++) {
             $s_lastPath = $s_path;
-            
+
             if (! empty($s_path)) {
                 $s_path . '/';
             }
             $s_path .= $a_path[$i];
-            
+
             try {
                 $element = $this->getBlock($s_path);
             } catch (\XMLException $e) {
                 /* Block does not exist */
                 $element = $this->dom_document->createElement($a_path[$i], '');
                 $parent = $this->getBlock($s_lastPath);
-                
+
                 $parent = $parent->item(0);
                 $parent->appendChild($element);
             }
@@ -254,13 +254,13 @@ class Xml extends Service
     public function save($s_file)
     {
         \youconix\core\Memory::type('String', $s_file);
-        
+
         $s_dir = dirname($s_file);
-        
+
         if (! is_writable($s_dir) && (! file_exists($s_dir) || ! is_writable($s_file))) {
             throw new \Exception("Can not write to " . $s_file . '. Check the permissions.');
         }
-        
+
         $this->dom_document->save($s_file);
     }
 
@@ -274,14 +274,14 @@ class Xml extends Service
     public function exists($s_path)
     {
         $s_path = $this->getRealPath($s_path);
-        
+
         $a_return = $this->obj_document->query("//" . $s_path);
-        
+
         if ($a_return->length < 1) {
             /* Part not found */
             return false;
         }
-        
+
         return true;
     }
 
@@ -300,7 +300,7 @@ class Xml extends Service
     public function insert($s_path, $a_keys, $a_values)
     {
         $s_text = $this->get($s_path);
-        
+
         if (is_array($a_keys)) {
             for ($i = 0; $i < count($a_keys); $i ++) {
                 $s_text = str_replace('[' . $a_keys[$i] . ']', $a_values[$i], $s_text);
@@ -308,7 +308,7 @@ class Xml extends Service
         } else {
             $s_text = str_replace('[' . $a_key . ']', $a_values, $s_text);
         }
-        
+
         return $s_text;
     }
 
@@ -323,24 +323,35 @@ class Xml extends Service
     protected function getRealPath($s_path)
     {
         \youconix\core\Memory::type('String', $s_path);
-        
+
         if (substr($s_path, - 1) == '/') {
             throw new \XMLException('Invalid XML query : ' . $s_path);
         }
-        
+
         if (empty($this->s_startTag)) {
             return $s_path;
         }
-        
+
         $i_length = strlen($this->s_startTag);
         if (substr($s_path, 0, $i_length) != $this->s_startTag) {
             $s_path = $this->s_startTag . '/' . $s_path;
         }
-        
+
         if (substr($s_path, - 1) == '/') {
             $s_path = substr($s_path, 0, - 1);
         }
-        
+
         return $s_path;
+    }
+
+    /**
+     * Validate the DOM Document against a specified XSD schema file.
+     * @param string $s_path            The path to the schema file, on-system.
+     * @param int [optional] $flags     The flags to apply to the validation process.
+     * @return boolean
+     */
+    public function schemaValidate($s_path, $flags = null)
+    {
+        return $this->dom_document->schemaValidate($s_path, $flags);
     }
 }
