ab5084aae9dd8eb3855ddf374016af2b3098b367
Rachelle Scheijen <rachelleScheijen@gmail.com>
2017-01-30 00:13:13 +0100

    added more default mime types

--- services/DefaultMimeTypes.php
+++ services/DefaultMimeTypes.php
@@ -39,6 +39,14 @@ class DefaultMimeTypes extends Service
     $this->a_extensions['csv'] = ['text/comma-separated-values', 'text/csv', 'text/plain',
         'application/csv', 'application/excel', 'application/vnd.ms-excel', 'application/vnd.msexcel',
         'text/anytext'];
+    $this->a_extensions['jpg'] = ['image/jpg'];
+    $this->a_extensions['jpeg'] = ['image/jepg'];
+    $this->a_extensions['gif'] = ['image/gif'];
+    $this->a_extensions['png'] = ['image/png'];
+    $this->a_extensions['tiff'] = ["image/tiff"];
+    $this->a_extensions['bmp'] = ["image/bmp"];
+    $this->a_extensions['svg'] = ["image/svg", "image/svg+xml"];
+    $this->a_extensions['webp'] = ['image/webp'];
   }
 
   public function __get($s_extension)
